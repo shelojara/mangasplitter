c23d0e093f04131b955af2e448a8bb861f98e8b1
Marcelo Jara <marcelo.jara.alm@gmail.com>
2025-06-28 15:49:59 -0400

    add multiple device support, and do not split volume covers

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+from enum import Enum
 from multiprocessing import Pool
 import zipfile
 import os
@@ -6,14 +7,26 @@ from PIL import Image, ImageChops
 import shutil
 from tqdm import tqdm
 
+
+class Device(Enum):
+    kindle = (1246, 1648)  # kindle paperwhite 4
+    kobo = (1072, 1448)  # kobo clara colour
+
+    def __str__(self):
+        return self.name
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--input", type=str)
 parser.add_argument("--device-width", type=int, default=1072)  # kindle: 1246
 parser.add_argument("--device-height", type=int, default=1448)  # kindle: 1648
 parser.add_argument("-w", "--workers", type=int, default=6)
 parser.add_argument("-q", "--quality", type=int, default=50)
+parser.add_argument("-d", "--device", type=str, choices=[d.name for d in Device])
 args = parser.parse_args()
 
+if args.device is not None:
+    args.device_width, args.device_height = Device[args.device].value
 
 # desired dimensions.
 ratio = args.device_width / args.device_height
@@ -39,7 +52,7 @@ def process_file(root: str, file: str):
         # unsupported file type.
         return
 
-    image = trim(Image.open(os.path.join(root, file)))
+    image = trim(Image.open(os.path.join(root, file)).convert("RGB"))
     width, height = image.size
     ratio = height / width
 
@@ -49,6 +62,9 @@ def process_file(root: str, file: str):
     if height <= WIDTH:
         # nothing to do, original image already fits.
         return
+
+    # remove original file.
+    os.remove(os.path.join(root, file))
 
     # extract parts.
     top = image.crop((0, 0, HEIGHT, WIDTH))
@@ -60,15 +76,16 @@ def process_file(root: str, file: str):
     bottom = image.crop((0, height - WIDTH, HEIGHT, height))
     bottom.save(os.path.join(root, f"{filename}.3.jpg"), optimize=True, quality=QUALITY)
 
-    # remove original file.
-    os.remove(os.path.join(root, file))
-
 
 def zip_output(file: str):
     print("Creating archive...")
+    suffix = "_processed"
+    if args.device is not None:
+        suffix = f"_{args.device}"
+
     filename, _ = os.path.splitext(file)
-    shutil.make_archive(filename + "_processed", "zip", "output")
-    shutil.move(filename + "_processed.zip", filename + "_processed.cbz")
+    shutil.make_archive(filename + suffix, "zip", "output")
+    shutil.move(filename + suffix + ".zip", filename + suffix + ".cbz")
     shutil.rmtree("output")
 
 
@@ -84,9 +101,16 @@ def main():
         for file in files:
             all_files.append((root, file))
 
+    seen_first = False
+
     with tqdm(total=len(all_files)) as pbar:
         p = Pool(processes=args.workers)
-        for root, file in all_files:
+        for root, file in sorted(all_files):
+            # do not process the first file, it's the volume cover.
+            if not seen_first:
+                seen_first = True
+                continue
+
             p.apply_async(
                 process_file,
                 (root, file),
